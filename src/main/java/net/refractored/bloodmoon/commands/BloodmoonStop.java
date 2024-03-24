404c55ef13857be70aabe77802513f5d4ed98432
refractored <me@refractored.net>
2024-03-24 12:30:10 -0500

    feat(BloodmoonStop): Add a world parameter & update reply method

--- src/main/java/net/refractored/bloodmoon/commands/BloodmoonStop.java
+++ src/main/java/net/refractored/bloodmoon/commands/BloodmoonStop.java
@@ -1,12 +1,14 @@
 package net.refractored.bloodmoon.commands;
 
 import net.refractored.bloodmoon.Bloodmoon;
+import net.refractored.bloodmoon.managers.BloodmoonManager;
 import net.refractored.bloodmoon.readers.LocaleReader;
 import net.refractored.bloodmoon.PeriodicNightCheck;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Description;
+import revxrsal.commands.annotation.Optional;
 import revxrsal.commands.bukkit.BukkitCommandActor;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
@@ -15,26 +17,42 @@ public class BloodmoonStop {
     @CommandPermission("bloodmoon.admin.stop")
     @Description("Stops the bloodmoon")
     @Command("bloodmoon stop")
-    public void bloodmoonStart(BukkitCommandActor actor) {
-
-        Player player = actor.getAsPlayer();
-        World playerWorld = player.getWorld();
-        if (Bloodmoon.GetInstance().getConfigReader(playerWorld).GetPermanentBloodMoonConfig())
+    public void bloodmoonStart(BukkitCommandActor actor, @Optional World targetworld) {
+        LocaleReader localeReader = Bloodmoon.GetInstance().getLocaleReader();
+        World world;
+        if (targetworld == null) {
+            if (actor.isConsole()){
+                actor.reply("&cYou must specify a world.");
+                return;
+            }
+            world = actor.getAsPlayer().getWorld();
+        } else {
+            world = targetworld;
+        }
+        if (world.getEnvironment() != World.Environment.NORMAL) {
+            actor.reply("&cTarget must be a overworld.");
+            return;
+        }
+        if (Bloodmoon.GetInstance().getConfigReader(world).GetPermanentBloodMoonConfig())
         {
-            LocaleReader.MessageLocale("CannotStopBloodMoon", null, null, player);
+            actor.reply(localeReader.GetLocaleString("CannotStopBloodMoon"));
             return;
         }
 
-        PeriodicNightCheck nightCheck = PeriodicNightCheck.GetPeriodicNightCheck(playerWorld);
+        PeriodicNightCheck nightCheck = PeriodicNightCheck.GetPeriodicNightCheck(world);
         if (nightCheck == null)
         {
-            LocaleReader.MessageLocale("NoBloodMoonInWorld", null, null, player);
+            actor.reply(localeReader.GetLocaleString("NoBloodMoonInWorld"));
             return;
         }
 
+        if (!BloodmoonManager.GetActuator(world).isInProgress())
+        {
+            actor.reply("&cA bloodmoon is not in progress in this world.");
+            return;
+        }
         nightCheck.SetCheckAfter(0);
         nightCheck.SetDaysRemaining(nightCheck.GetBloodMoonInterval() - 1);
-        playerWorld.setTime(0);
-        return;
+        world.setTime(0);
     }
 }
